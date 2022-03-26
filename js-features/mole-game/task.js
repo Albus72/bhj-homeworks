2d9894cd45e3b6ec6b65bd079facbd34b31e9e18
Albus72 <albus72@tut.by>
2022-03-26 23:34:18 +0300

    Доработал функцию вывода сообщений

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,7 +2,7 @@ let hit = document.getElementById('dead');
 let miss = document.getElementById('lost');
 
 for (let index = 1; index < 10; index++) { 
-  getHole = (index) => document.getElementById(`hole${index}`);
+  getHole = index => document.getElementById(`hole${index}`);
   getHole(index).onclick = function () {
     let alertMessage;
     if (getHole(index).classList.contains('hole_has-mole')) {
@@ -12,11 +12,11 @@ for (let index = 1; index < 10; index++) {
     }
     if (miss.textContent === '5') {
         alertMessage = 'Не повезло. Попробуйте еще раз.';
-        getAlertMessage(alertMessage);
+        setTimeout(getAlertMessage, 100);
     }
     if (hit.textContent === '10') {
         alertMessage = 'Поздравляю! Вы настоящий кротобой.';
-        getAlertMessage(alertMessage);
+        setTimeout(getAlertMessage, 100);
     }
     function getAlertMessage() {
       alert(alertMessage);
