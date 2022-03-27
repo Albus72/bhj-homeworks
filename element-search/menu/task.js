5cdc47001aa7ec22d099ab2ad71db7ee8a086be3
Albus72 <albus72@tut.by>
2022-03-27 20:11:17 +0300

    Задача 2 готова

---
+++ element-search/menu/task.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body');
+const menu = document.querySelector('.menu');
+body.innerHTML =
+  body.innerHTML +
+  '<br><br><br><br></br>' +
+  '<ul class="menu menu_main">' +
+  menu.innerHTML +
+  '</ul>';
+const menuLink = document.querySelectorAll('.menu__link');
+
+for (let i = 0; i < menuLink.length; i++) {
+  let menuItem = menuLink[i].closest('.menu__item');
+  let menuSub = menuItem.querySelector('.menu_sub');
+  let menuClosest = menuLink[i].closest('.menu');
+  let menuSubAll = menuClosest.querySelectorAll('.menu_sub');
+
+  if (menuSub) {
+    menuLink[i].onclick = function () {
+      if (!menuSub.classList.contains('menu_active')) {
+        menuSubAll.forEach((element) => element.classList.remove('menu_active'));
+        menuSub.classList.add('menu_active');
+      } else {
+        menuSub.classList.remove('menu_active');
+      }
+      return false;
+    };
+  }
+}
