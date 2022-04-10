19ba48f8ae00a972ec5df9d211370c2f62ae0291
Albus72 <albus72@tut.by>
2022-04-10 22:30:05 +0300

    Задача 1 начало

---
+++ dom/reveal/task.js
@@ -0,0 +1,13 @@
+let reveals = Array.from(document.querySelectorAll('.reveal'));
+
+window.addEventListener('scroll', function() {
+    let sizeWindow = window.innerHeight;
+    for (let i = 0; i < reveals.length; i++) {
+        let topElement = reveals[i].getBoundingClientRect().top;
+        let bottomElement = reveals[i].getBoundingClientRect().bottom;
+        if ((topElement > 0 || (bottomElement > 0 && bottomElement < sizeWindow)) && topElement < sizeWindow && !reveals[i].classList.contains('reveal_active')) {
+            reveals[i].classList.add('reveal_active');
+            console.log(`элемент №${i + 1} загружен`);
+        }
+    }  
+});
