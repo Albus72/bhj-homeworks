df46d5798fdb1f9a933252575d74d0a04f3fbd2f
Albus72 <albus72@tut.by>
2022-04-11 20:03:51 +0300

    Задача 2 начало

---
+++ dom/ads/task.js
@@ -0,0 +1,3 @@
+const content = document.querySelector('.content');
+const card = document.querySelector('.card');
+content.innerHTML = content.innerHTML + '<div class="card">' + card.innerHTML + '</div>';
