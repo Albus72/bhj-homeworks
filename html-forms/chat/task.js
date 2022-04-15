ba7113362cc54ec8840a4d8917885fb75449ec56
Albus72 <albus72@tut.by>
2022-04-15 20:21:01 +0300

    Задача 1 начало

---
+++ html-forms/chat/task.js
@@ -0,0 +1,5 @@
+const chatOpen = document.querySelector('.chat-widget');
+const chatScroll = chatOpen.querySelector('.chat-widget__messages-container');
+const chatInput = document.getElementById('chat-widget__input');
+let counter = 0;
+let timerID;
