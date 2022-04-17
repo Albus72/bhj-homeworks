27a9ea3419769685325d3ead599fce884d5e076e
Albus72 <albus72@tut.by>
2022-04-17 17:30:59 +0300

    Задача 1 исправлена после проверки

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -3,9 +3,8 @@ let reveals = Array.from(document.querySelectorAll('.reveal'));
 window.addEventListener('scroll', function() {
     let sizeWindow = window.innerHeight;
     for (let i = 0; i < reveals.length; i++) {
-        let topElement = reveals[i].getBoundingClientRect().top;
-        let bottomElement = reveals[i].getBoundingClientRect().bottom;
-        if ((topElement > 0 || (bottomElement > 0 && bottomElement < sizeWindow)) && topElement < sizeWindow && !reveals[i].classList.contains('reveal_active')) {
+        let {bottom, top} = reveals[i].getBoundingClientRect();
+        if ((top > 0 || (bottom > 0 && bottom < sizeWindow)) && top < sizeWindow && !reveals[i].classList.contains('reveal_active')) {
             reveals[i].classList.add('reveal_active');
         }
     }  
