1c4b45a855d66f74d4fff26d7b62d6f362eb7d26
Albus72 <albus72@tut.by>
2022-04-22 16:24:26 +0300

    Задача 2 начало

---
+++ document-structure/todo/task.js
@@ -0,0 +1,4 @@
+const taskInput = document.getElementById('task__input');
+const tasksAdd = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+let localStorageNotes = JSON.parse(localStorage.getItem("notes"));
