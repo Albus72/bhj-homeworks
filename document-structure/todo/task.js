54ce22869515a5774c8f66fab140bbf385f9cf57
Albus72 <albus72@tut.by>
2022-04-26 21:09:05 +0300

    Задача 2 исправлена после проверки

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,20 +1,20 @@
 const taskInput = document.getElementById('task__input');
 const tasksAdd = document.getElementById('tasks__add');
 const tasksList = document.getElementById('tasks__list');
-let localStorageNotes = JSON.parse(localStorage.getItem('notes'));
+let notes = JSON.parse(localStorage.getItem('notes'));
+let taskRemove = tasksList.getElementsByClassName('task__remove');
 
-if (localStorageNotes != null) {  
-    for (let i = 0; i < localStorageNotes.length; i++) {
-        htmlNoteAdd(localStorageNotes[i]);
-        taskRemoteFunc();
+if (notes != null) {  
+    for (let i = notes.length - 1; i >= 0; i--) {
+        htmlNoteAdd(notes[i]);
+        taskRemoteFunc(taskRemove[i]);
     }
 };
 
-function taskRemoteFunc() {
-    let taskRemove = tasksList.querySelectorAll('.task__remove');
-    taskRemove[0].addEventListener('click', function(event) {
+function taskRemoteFunc(node) {
+    node.addEventListener('click', function(event) {
         event.preventDefault();
-        let task = taskRemove[0].closest('.task');
+        let task = node.closest('.task');
         let taskTitle = task.querySelector('.task__title').innerText;
         updateLocalStorage(null, taskTitle);
         task.remove();
@@ -26,13 +26,12 @@ tasksAdd.addEventListener('click', function(event) {
     if (taskInput.value) {
         htmlNoteAdd(taskInput.value);
         updateLocalStorage(taskInput.value, null);
-        taskRemoteFunc();
+        taskRemoteFunc(taskRemove[taskRemove.length - 1]);
         taskInput.value = '';
     }
 });
 
 function updateLocalStorage(noteAdd, noteDel) {
-    let notes = JSON.parse(localStorage.getItem("notes"));
     if (notes != null && noteAdd != null) {notes.unshift(noteAdd)};
     if (notes === null && noteAdd != null) {notes = [noteAdd]};
     if (notes != null && noteDel != null) {notes.splice(notes.indexOf(noteDel), 1)};
@@ -40,12 +39,11 @@ function updateLocalStorage(noteAdd, noteDel) {
 }
 
 function htmlNoteAdd(note) {
-    let html = `
-    <div class="task">
-        <div class="task__title">
-            ${note}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-    </div>`;
-    tasksList.insertAdjacentHTML('afterBegin', html);
+    let div = document.createElement('div');
+    div.className = "task";
+    div.innerHTML = `<div class="task__title">
+        ${note}
+    </div>
+    <a href="#" class="task__remove">&times;</a>`;
+    tasksList.appendChild(div);
 }
