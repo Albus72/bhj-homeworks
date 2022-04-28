d838cf7a8814d250244548e3f04a287b2b262dc2
Albus72 <albus72@tut.by>
2022-04-28 12:09:41 +0300

    Задача 2 начало

---
+++ async-requests/poll/task.js
@@ -0,0 +1,5 @@
+const pollTitle = document.getElementById('poll__title');
+const pollAnswers = document.getElementById('poll__answers');
+const body = document.querySelector('body');
+const xhr = new XMLHttpRequest();
+
