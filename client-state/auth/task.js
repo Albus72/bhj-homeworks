9afe550d543c2f814d4cdb0ec44d0f9944eb62e6
Albus72 <albus72@tut.by>
2022-05-01 14:31:24 +0300

    Задача 3 начало

---
+++ client-state/auth/task.js
@@ -0,0 +1,5 @@
+const signin = document.getElementById('signin');
+const form = document.getElementById('signin__form');
+const welcome = document.getElementById('welcome');
+const userId = document.getElementById('user_id');
+const localUserId = JSON.parse(localStorage.getItem("userId"));
